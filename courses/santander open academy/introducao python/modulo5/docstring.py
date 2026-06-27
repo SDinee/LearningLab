708357fd0d4e6ab2d7ff24b37020770f0ef241d7
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 16:57:10 -0300

    exemplos com funções

---
+++ courses/santander open academy/introducao python/modulo5/docstring.py
@@ -0,0 +1,14 @@
+def area_retangulo(base, altura):
+    """
+    Calcula a área de um retângulo.
+
+
+    Args:
+        base (float): A base do retângulo.
+        altura (float): A altura do retângulo.
+
+
+    Returns:
+        float: A área do retângulo.
+    """
+    return base * altura
---
+++ courses/santander open academy/introducao python/modulo5/parametrosfuncao.py
@@ -0,0 +1,6 @@
+def saudacao(nome):
+    print(f"Olá, {nome}!")
+    
+saudacao("João")  # Imprime "Olá, João!"
+saudacao("Maria")  # Imprime "Olá, Maria!"
+ 
