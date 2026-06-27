ed637897632018538e8209c71ef0a3d5a29e6243
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 04:01:15 -0300

    métodos com tuplas

--- courses/santander open academy/introducao python/modulo4/lista.py
+++ courses/santander open academy/introducao python/modulo4/lista.py
@@ -8,6 +8,14 @@ print(frutas[-1])  # Imprime "laranja"
 print(frutas[-2])  # Imprime "banana"
 print(frutas[-3])  # Imprime "maçã"
 
+"""
+Embora as tuplas sejam imutáveis, Python fornece vários métodos úteis para trabalhar com elas:
+
+count(elemento): devolve o número de vezes que um elemento aparece na tupla. 
+index(elemento): devolve o índice da primeira aparição de um elemento na tupla. Opcionalmente, pode-se especificar o início e fim da busca. 
+len(tupla): embora não seja um método de tupla propriamente dito, esta função incorporada devolve o comprimento da tupla.
+"""
+
 frutas.append("pera")
 print(frutas)  # Imprime ["maçã", "banana", "laranja", "pera"]
 
