82f57dfb500c8951bb92606e943d67cad97b36c1
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 03:36:34 -0300

    exemplos de operadores de comparação

---
+++ courses/santander open academy/introducao python/modulo2/comparacao.py
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+
+
+igual = a == b   # False
+diferente = a != b   # True
+maior = a > b   # True
+menor = a < b   # False
+maiorigual = a >= b   # True
+menorigual = a <= b   # False
