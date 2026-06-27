c18fa245c60b1ed0e9c27c31d1e0cd336d421fd5
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 03:52:38 -0300

    exemplo de uma lista de compreensão

---
+++ courses/santander open academy/introducao python/modulo4/listacompreensao.py
@@ -0,0 +1,5 @@
+números = [1, 2, 3, 4, 5]
+quadrados = [x ** 2 for x in números if x % 2 == 0]
+print(quadrados)  # Imprime [4, 16]
+
+# É criada uma nova lista chamada quadrados, que contém os quadrados dos números pares da lista números. A expressão x ** 2 eleva cada elemento ao quadrado, e a condição if x % 2 == 0 filtra apenas os números pares.
