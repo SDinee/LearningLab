5aef504def604f911154673a17c208e4d3d2510f
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 03:49:23 -0300

    exemplos de loops e controladores de loops

---
+++ courses/santander open academy/introducao python/modulo3/controlebreak.py
@@ -0,0 +1,13 @@
+contador = 0
+
+
+while True:
+
+    print(contador)
+    contador += 1
+
+
+    if contador == 5:
+        break
+    
+# A instrução break é utilizada para sair prematuramente de um loop, independentemente da condição. Quando um break é encontrado, o loop é interrompido e o fluxo de execução continua com a próxima instrução fora do loop.
---
+++ courses/santander open academy/introducao python/modulo3/controlepass.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    pass
+
+# A instrução pass é uma operação nula que não faz nada. É utilizada como um marcador de posição quando uma instrução é sintaticamente necessária, mas nenhuma ação é desejada
