1aa0320aad9dae5617ff3b7ff9ef1a331fde76a4
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 04:05:45 -0300

    exemplo com conjuntos

---
+++ courses/santander open academy/introducao python/modulo4/conjunto.py
@@ -0,0 +1,48 @@
+# Os conjuntos suportam operações matemáticas de conjuntos, como a união (|), a interseção (&), a diferença (-) e a diferença simétrica (^).
+
+conjunto1 = {1, 2, 3}
+conjunto2 = {3, 4, 5}
+
+
+uniao = conjunto1 | conjunto2
+print(uniao)  # Imprime {1, 2, 3, 4, 5}
+
+
+intersecao = conjunto1 & conjunto2
+print(intersecao)  # Imprime {3}
+
+
+diferenca = conjunto1 - conjunto2
+print(diferenca)  # Imprime {1, 2}
+
+
+diferenca_simetrica = conjunto1 ^ conjunto2
+print(diferenca_simetrica)  # Imprime {1, 2, 4, 5}
+
+
+"""
+Os conjuntos em Python têm vários métodos incorporados para manipular e acessar os elementos. Alguns métodos comuns são:
+
+add(elemento): adiciona um elemento ao conjunto.
+remove(elemento): remove um elemento do conjunto. Se o elemento não existir, gera um erro.
+discard(elemento): remove um elemento do conjunto se estiver presente. Se o elemento não existir, não faz nada.
+clear(): remove todos os elementos do conjunto.
+"""
+
+frutas = {"maçã", "banana", "laranja"}
+
+
+frutas.add("pera")
+print(frutas)  # Imprime {"maçã", "banana", "laranja", "pera"}
+
+
+frutas.remove("banana")
+print(frutas)  # Imprime {"maçã", "laranja", "pera"}
+
+
+frutas.discard("uva")
+print(frutas)  # Imprime {"maçã", "laranja", "pera"}
+
+
+frutas.clear()
+print(frutas)  # Imprime set()
