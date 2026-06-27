b6b1c2fbfd28860e2cca6ea64398b64ee8e1e8f5
Sidne <sidne.correiacabo@gmail.com>
2026-06-27 03:57:39 -0300

    exemplo com tuplas

---
+++ courses/santander open academy/introducao python/modulo4/tuplas.py
@@ -0,0 +1,14 @@
+minha_tupla = (1, 2, 3, 2, 4, 2)
+
+
+print (minha_tupla.index(2))   # Saída: 1
+
+print (minha_tupla.index(2, 2))   #Saída: 3
+
+print (minha_tupla.index(2, 2, 4))   #Saída: 3
+
+ponto = (3, 4)
+
+print(ponto[0])  # Imprime 3
+
+print(ponto[1])  # Imprime 4
