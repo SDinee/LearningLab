c8bfcc3bf30dab16c02545e1103993c8b65c2bbd
Sidne <sidne.correiacabo@gmail.com>
2026-07-25 12:35:02 -0300

    declaração de variáveis

---
+++ research/java/sintaxe/variaveis/declararvar.java
@@ -0,0 +1,18 @@
+package research.java.sintaxe.variaveis;
+
+
+// Primeiro método de declaração de variáveis em Java
+public class declararvar {
+    int idade = 20; // declarando e inicializando a variável idade
+    String nome = "João"; // declarando e inicializando a variável nome
+
+    // tipo NomeVariavel = valor;
+}
+
+// Segundo método de declaração de variáveis em Java
+class Metododois {
+    //var idade = 20; // declarando e inicializando a variável idade
+    //var nome = "João"; // declarando e inicializando a variável nome
+}
+// var é uma palavra reservada do Java que permite a inferência de tipos, ou seja, o compilador determina o tipo da variável com base no valor atribuído a ela.
+// não ativo no visual studio code
