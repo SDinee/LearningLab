6518ac9cb8245951e9df84008789b67b24ee5a21
Sidne <sidne.correiacabo@gmail.com>
2026-07-25 12:47:23 -0300

    tipos de dados

---
+++ research/java/tiposprimitivos/tipos.java
@@ -0,0 +1,21 @@
+package research.java.tiposprimitivos;
+
+public class tipos {
+    // inteiros = 100, 232323, 255, 120, 1
+    /*
+    byte => 8 bits => -128 a 127
+    short => 16 bits => -32.768 a 32.767
+    int => 32 bits => -2.147.483.648 a 2.147.483.647
+    long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+    // decimais = 10.5, 20.0, 30.99, 100.01
+    float => 32 bits => precisão de 6 a 7 dígitos => simples
+    double => 64 bits => precisão de 15 dígitos => dupla
+
+    String = representar palavras, frases, textos => "Olá, mundo!", "Java é incrível!", "Aprendendo Java"
+    char = representar um único caractere => 'A', 'B', 'C', '1', '2', '3'
+    // string usa aspas duplas, char usa aspas simples
+
+    boolean = representar valores lógicos => true, false 
+    */
+}
