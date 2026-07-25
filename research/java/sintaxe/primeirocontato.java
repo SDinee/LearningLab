78fcad96f20e998b2d9d42fac631d08ed317160c
Sidne <sidne.correiacabo@gmail.com>
2026-07-25 12:22:37 -0300

    primeiro contato com java

---
+++ research/java/sintaxe/primeirocontato.java
@@ -0,0 +1,11 @@
+package research.java.sintaxe;
+/* serve para dizer em qual pacote (package) a classe está. O Java exige isso quando o arquivo está dentro de uma estrutura de pastas que representa um pacote. */
+
+public class primeirocontato {
+
+}
+/* classe public deve ter o mesmo nome do arquivo .java */
+
+class Carro {
+    
+}
