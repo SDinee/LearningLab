428058b2b6c6d8b963f8f8727a6fece0e6a39cf6
Sidne <sidne.correiacabo@gmail.com>
2026-09-04 20:32:22 -0300

    feat(c#): adiciona exemplos com funções

---
+++ research/c#/funcoes/Program.cs
@@ -0,0 +1,12 @@
+﻿/*void pois ela não retorna nada, caso contrário, usaria 'static string Saudacao(String nome)' */
+static void Saudacao(String nome, String sobrenome = "Silva") {
+    Console.WriteLine("Olá, " + nome + " " + sobrenome + "!");
+}
+void Idade(int idade) {
+    Console.WriteLine("Você tem " + idade + " anos.");
+}
+
+Saudacao("Clebinho");
+Idade(25);
+
+/*Funções e métodos são a mesma coisa*/
