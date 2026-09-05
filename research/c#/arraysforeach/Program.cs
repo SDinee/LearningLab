4861919583303779a854526e230b285fdb8b58b9
Sidne <sidne.correiacabo@gmail.com>
2026-09-04 21:05:07 -0300

    feat(c#): adiciona exemplos com arrays e foreach

---
+++ research/c#/arraysforeach/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] frutas = { "Maçã", "Banana", "Laranja", "Uva" };
+
+foreach (string fruta in frutas) {
+    Console.WriteLine("Fruta: " + fruta);
+}
+
+Console.WriteLine(frutas[2]); // Acessando o elemento na posição 2 do array (Laranja)
+
+int[] numeros = { 1, 2, 3, 4, 5 };
+
+foreach (int numero in numeros) {
+    Console.WriteLine(numero);
+}
