f887d8a24312ffa19d541aac23468f174ed958e1
Sidne <sidne.correiacabo@gmail.com>
2026-09-11 22:22:18 -0300

    feat: Adiciona criação de objeto pessoa

---
+++ research/javascript/variaveis/objeto.js
@@ -0,0 +1,8 @@
+const pessoa = {
+    Nome: "Sidne",
+    Idade: 21,
+    Cidade: "Rio de Janeiro",
+    Estudante: true
+}
+                                                    //operador ternário em JavaScript: condição ? valorSeVerdadeiro : valorSeFalso
+console.log(pessoa.Nome, pessoa.Idade, pessoa.Cidade, pessoa.Estudante ? "Sim" : "Não"); // acessando as propriedades do objeto
