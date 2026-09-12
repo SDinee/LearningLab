196f4c5e2683fd2d12c2aa60dafabc8a4942176d
Sidne <sidne.correiacabo@gmail.com>
2026-09-11 22:28:46 -0300

    feat: adiciona prática com arrays

---
+++ research/javascript/variaveis/arrays.js
@@ -0,0 +1,5 @@
+const frutas = ["maçã", "banana", "laranja"];
+
+console.log(frutas[0]); // Acessando o primeiro elemento do array
+console.log(frutas[1]); // Acessando o segundo elemento do array
+console.log(frutas[2]); // Acessando o terceiro elemento do array  
