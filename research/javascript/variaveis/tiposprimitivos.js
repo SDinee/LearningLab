92d1c957e71c8bfc810bb711b70211b9f907835b
Sidne <sidne.correiacabo@gmail.com>
2026-09-11 22:21:30 -0300

    feat: Adiciona exemplo de tipos com javascript

---
+++ research/javascript/variaveis/tiposprimitivos.js
@@ -0,0 +1,8 @@
+let nome = "Maria";     // string
+let idade = 25;         // number
+let estudante = true;   // boolean
+
+let a;           // declarada, mas sem valor → undefined
+let b = null;    // valor definido como "nada" → null
+
+
