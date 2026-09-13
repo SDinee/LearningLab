905ad485efb90f3fc19623fb4627459f1eb1c135
Sidne <sidne.correiacabo@gmail.com>
2026-09-13 16:23:40 -0300

    feat(js): adiciona arquivo com exemplo de função dentro de variável

---
+++ research/javascript/funcoes/funcaovariavel.js
@@ -0,0 +1,19 @@
+// A variável "falar" recebe uma função.
+// A função não possui nome, por isso é anônima.
+const falar = function() {
+
+    // Retorna a string "Oi".
+    return "Oi";
+};
+
+// Como "falar" guarda uma função,
+// podemos executar essa função usando ().
+console.log(falar()); // Oi
+
+
+// Arrow function
+// A variável "falar2" recebe uma arrow function.
+// A arrow function é uma forma mais curta de escrever funções anônimas.
+const falar2 = () => "Oi";
+
+console.log(falar2()); // Oi
