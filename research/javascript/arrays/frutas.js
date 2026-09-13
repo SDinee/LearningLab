1dfe5e0dd3463cc8347c98c1ca484743066b45fc
Sidne <sidne.correiacabo@gmail.com>
2026-09-13 19:39:48 -0300

    feat(js): adiciona um array com frutas

---
+++ research/javascript/arrays/frutas.js
@@ -0,0 +1,4 @@
+let frutas = ["maçã", "banana", "uva"];
+console.log(frutas[0]); // "maçã"
+console.log(frutas[2]); // "uva"
+console.log(frutas.length); // length conta quantos elementos existem no array, no caso 3
